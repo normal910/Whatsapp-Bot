c4b2ba71b1b4f5698eac27d25cc5098cb12f6971
normal910 <Eggycapibara910@users.github.com>
2021-03-22 19:08:26 -0300

    Updated ChromeDriver path settings

--- reply.py
+++ reply.py
@@ -1,11 +1,14 @@
+path = 'C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe' #Path to the ChromeDriver executable
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome('C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe')
+driver = webdriver.Chrome(path)
 driver.get("https://web.whatsapp.com/")
 
 input("Press anything after QR scan")
+#you can set a sleep if requierd
 time.sleep(5)
 
 
@@ -22,31 +25,32 @@ for name in names:
     msg_got = driver.find_elements_by_css_selector("span.selectable-text.invisible-space.copyable-text")
     msg = [message.text for message in msg_got]
 
-   
-    if msg[0] == "Hello!":
+try:
+
+     if msg[0] == "Hello!":
    
       xpath = './/div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]'
       reply = driver.find_element_by_xpath(xpath)
-      #("_2S1VP.copyable-text.selectable-text")reply.clear()
+      
       reply.send_keys("Hello! message recieved correctly") 
       reply.send_keys(Keys.RETURN)
 
 
 except IndexError:
- print("IndexError")
- reply = driver.find_element_by_xpath(xpath)
- reply.clear()
- reply.send_keys("The script ran into a IndexError!")
- reply.send_keys(Keys.RETURN)
+
+     print("IndexError")
+     reply = driver.find_element_by_xpath(xpath)
+     reply.clear()
+     reply.send_keys("The script ran into a IndexError!")
+     reply.send_keys(Keys.RETURN)
 
 
 
-
-       else:
-       reply = driver.find_element_by_xpath(xpath)
-       reply.clear()
-       reply.send_keys("your message could not be read correctly")
-       reply.send_keys(Keys.RETURN)
+else:
+    reply = driver.find_element_by_xpath(xpath)
+    reply.clear()
+    reply.send_keys("Your message could not be read correctly")
+    reply.send_keys(Keys.RETURN)
 
 finally:
    reply = driver.find_element_by_xpath(xpath)
