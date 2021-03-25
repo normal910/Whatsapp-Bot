c0d620c51d161163e995a883cd7f2ba6eb5960fc
normal910 <Eggycapibara910@users.github.com>
2021-03-25 08:25:43 -0300

    Fixed bug while getting messages

--- reply.py
+++ reply.py
@@ -12,7 +12,7 @@ input("Press anything after QR scan")
 time.sleep(5)
 
 
-names = ["ContactName"] #Your Chat goes there <-
+names = ["Admin"] #Your Chat goes there <-
 
 for name in names:
 
@@ -22,9 +22,10 @@ for name in names:
     for i in range(1,3):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    msg_got = driver.find_elements_by_css_selector("span.selectable-text.invisible-space.copyable-text")
+    msg_got = driver.find_elements_by_class_name('_3ExzF')                                        #msg_got = driver.find_elements_by_css_selector("span.selectable-text.invisible-space.copyable-text")
     msg = [message.text for message in msg_got]
-
+    
+   
 try:
 
      if msg[0] == "Hello!":
@@ -34,26 +35,42 @@ try:
       
       reply.send_keys("Hello! message recieved correctly") 
       reply.send_keys(Keys.RETURN)
+      reply.clear()
+
+
+              
+     
+     if msg[0] == "Test":
+             xpath = './/div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]'
+             reply = driver.find_element_by_xpath(xpath)
+             reply.send_keys("Hello! message recieved correctly") 
+             reply.send_keys(Keys.RETURN)
+             reply.clear()
+
+
+     
+     else:
+
+         xpath = './/div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]'
+         reply = driver.find_element_by_xpath(xpath)
+         reply.clear()
+         reply.send_keys("Your message could not be read correctly")
+         reply.send_keys(Keys.RETURN)
+
 
 
 except IndexError:
-
+     xpath = './/div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]'
      print("IndexError")
      reply = driver.find_element_by_xpath(xpath)
      reply.clear()
      reply.send_keys("The script ran into a IndexError!")
      reply.send_keys(Keys.RETURN)
 
+      
 
-
-else:
-    reply = driver.find_element_by_xpath(xpath)
-    reply.clear()
-    reply.send_keys("Your message could not be read correctly")
-    reply.send_keys(Keys.RETURN)
-
-finally:
-   reply = driver.find_element_by_xpath(xpath)
-   reply.clear()
-   reply.send_keys("This message will be sent anyway!")
-   reply.send_keys(Keys.RETURN)
+#finally:
+ #  reply = driver.find_element_by_name('.//div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]')
+  # reply.clear()
+   #reply.send_keys("This message will be sent anyway!")
+   #reply.send_keys(Keys.RETURN)
